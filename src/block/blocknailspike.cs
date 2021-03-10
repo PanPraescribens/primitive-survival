64f7bd727759ba79dba6a28666eb731138e6f6ef
SpearAndFang <69551808+SpearAndFang@users.noreply.github.com>
2021-03-09 20:49:20 -0800

    2.5.6 updates

--- src/block/blocknailspike.cs
+++
@@ -1,9 +0,0 @@
-using Vintagestory.API.Common;
-
-public class BlockNailSpike : Block
-{
-    public override bool TryPlaceBlock(IWorldAccessor world, IPlayer byPlayer, ItemStack itemstack, BlockSelection blockSel, ref string failureCode)
-    {
-        return false;
-    }
-}
